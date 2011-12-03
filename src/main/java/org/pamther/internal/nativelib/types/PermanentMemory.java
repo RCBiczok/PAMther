eedd1f5aecb89c1a328b6f8298c7c6cb2af631bb
root <root@Ubuntu>
2011-12-03 18:38:59 +0100

    Add an subtype of Memory which does not delete memory on finalization

---
+++ src/main/java/org/pamther/internal/nativelib/types/PermanentMemory.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2011 Rudolf Biczok
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.pamther.internal.nativelib.types;
+
+import com.sun.jna.Memory;
+
+/**
+ * The {@link PermanentMemory} represents a {@link com.sun.jna.Pointer Pointer}
+ * to a block of memory. Instances of this class won't delete any allocated
+ * memory, so its up to the native code for cleaning up the memory block.
+ * 
+ * @author <a href="https://bitbucket.org/RCBiczok">Rudolf Biczok</a>
+ */
+class PermanentMemory extends Memory {
+
+	/**
+	 * Allocates some memory.
+	 * 
+	 * @param size
+	 *            number of <em>bytes</em> to allocate
+	 * @see com.sun.jna.Memory#Memory(long)
+	 */
+	PermanentMemory(long size) {
+		super(size);
+	}
+
+	/*
+	 * Uhhh this is evil hack .... (non-Javadoc)
+	 * 
+	 * @see com.sun.jna.Memory#finalize()
+	 */
+	protected void finalize() {
+	}
+}
